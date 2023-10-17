d8a92eb6e8e17d59d36edb74fe7bbfab909fe73c
DevoTrix <71916304+DevoTrix@users.noreply.github.com>
2023-10-16 19:36:03 -0700

    brief mockup of trying to get the data.
    Not done yet. gonna come back and do it later

---
+++ api_call_test.py
@@ -0,0 +1,41 @@
+import quantaq
+from quantaq.utils import to_dataframe
+import pandas as pd
+
+#input your apikey here... not sure if there is any safety issues of putting the api key into github, will look
+## into but for now im not gonna put it in.
+apiKey = ''
+
+#return a df
+def fetchAllRecent(client):
+
+    #empty list for later
+    data = []
+
+    #gather device infos currently limited to 10 cuz i dont wanna wait 5 years
+    devices = to_dataframe(client.devices.list(limit = 10))
+
+    #alternative
+    #devices = fetchActiveDevices(client)
+    
+    # get every serial number from devices
+    sn = devices.loc[:,'sn']
+    
+    #loop through each device and call data list limited to the most recent 1. 
+    for i in sn.index:
+        data.append( to_dataframe(client.data.list( sn = sn[i], sort = "timestamp,asc", limit = 1 ) ) )
+
+    #turns list of dataframes into 1 data fram i think.... this is the part that probably went wrong
+    data = pd.concat(data)
+    return data
+
+
+#gather active devices
+def fetchActiveDevices(client):
+    devices = to_dataframe(client.devices.list(filter="device_state,eq,ACTIVE"))
+    return devices
+
+#get the client
+client = quantaq.QuantAQAPIClient(api_key = apiKey)
+# print to test
+print(  fetchAllRecent(client) )
