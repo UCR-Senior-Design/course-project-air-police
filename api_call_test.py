c81011a3562ff663bdf4cd09eaf6becdf8f8111f
DevoTrix <71916304+DevoTrix@users.noreply.github.com>
2023-11-03 14:34:01 -0700

    did the specific devices for now

--- api_call_test.py
+++ api_call_test.py
@@ -12,23 +12,24 @@ apiKey = 'AOSIIFR5L7HM6KUISW2D4UFK'
 def fetchAllRecent(client):
 
     #empty list for later
-    data = []
 
     #gather device infos currently limited to 10 cuz i dont wanna wait 5 years
-    devices = to_dataframe(client.devices.list(limit = 10))
+    devices = to_dataframe(client.devices.list())
 
     #alternative
     #devices = fetchActiveDevices(client)
-    
+    data = []
     # get every serial number from devices
-    sn = devices.loc[:,'sn']
+    sn = ['MOD-PM-00635', 'MOD-PM-00689','MOD-PM-00691']
     
     #loop through each device and call data list limited to the most recent 1. 
-    for i in sn.index:
-        data.append( to_dataframe(client.data.list( sn = sn[i], sort = "timestamp,asc", limit = 1 ) ) )
+    for i in sn:
+        data.append(to_dataframe(client.data.list( sn = i, sort = "timestamp,desc", limit = 1 ) ) )
+        
 
     #turns list of dataframes into 1 data fram i think.... this is the part that probably went wrong
     data = pd.concat(data)
+    data = data[['pm10','pm25', 'sn']]
     return data
 
 
@@ -42,12 +43,27 @@ def getLongitudeLatitude(client, serialNumber):
     # need to add a statement here in case there are some problems
     return devices.loc[devices['sn'] == serialNumber, 'geo.lat':'geo.lon']
 def getAllLatitudeLongitude(client):
-    devices = to_dataframe(client.devices.list(limit = 10))
+    devices = to_dataframe(client.devices.list())
     return devices[['sn','geo.lat', 'geo.lon']]
 
 #get the client
+
 client = quantaq.QuantAQAPIClient(api_key = apiKey)
+who = client.whoami()
+print(who)
 # print to test
 print(fetchAllRecent(client))
+devices = to_dataframe(client.devices.list())
+# print(devices.columns)
+# print(devices)
 # print(getLongitudeLatitude(client, 'MOD-PM-00156'))
-print(  getAllLatitudeLongitude(client) )
+# print(  getAllLatitudeLongitude(client) )
+
+# teams = client.teams.list()
+# print(teams)
+
+data = client.data.list( sn = "MOD-PM-00696", sort = "timestamp,desc", limit = 1 ) 
+# print(data)
+data = to_dataframe(data)
+print(data[['pm10','pm25', 'sn']])
+# print(data)
