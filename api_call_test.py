f19547cfc1c1d01183dfc09e8fc002c42d6cb75c
Harrison Cooper <harrisoncooper@Harrisons-MacBook-Air.local>
2023-11-03 16:29:20 -0700

    added more comments to explain the code

--- api_call_test.py
+++ api_call_test.py
@@ -1,16 +1,16 @@
 #https://quant-aq.github.io/py-quantaq/ library documentation
-# eventually put this into a class
+# imports required to use the api
 import quantaq
 from quantaq.utils import to_dataframe
 import pandas as pd
 
-
 # input unique apikey here
 apiKey = '7QQN18AOIHAA15UD9SNKUIA1'
 
-
+# function that returns SN, particle count, and coordinates of devices passed in
 def fetchSensorData(client, sn):
     data = []
+    # appends raw data of each sensor in sn to a list
     for i in sn:
         data.append( to_dataframe(client.data.list( sn = i, sort="timestamp,desc", limit=1 ) ) )
 
@@ -19,7 +19,7 @@ def fetchSensorData(client, sn):
     data = data[['sn', 'pm10','pm25', 'geo.lat', 'geo.lon']]
     return data
 
-
+# driver function
 def main():
     #get the client
     client = quantaq.QuantAQAPIClient(api_key = apiKey)
