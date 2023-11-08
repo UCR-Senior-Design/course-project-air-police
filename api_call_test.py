d8818711dd5d963c06c2a44afd8c197bd3b75b95
DevoTrix <71916304+DevoTrix@users.noreply.github.com>
2023-11-07 17:54:56 -0800

    cleaned up and did stuff.

--- api_call_test.py
+++ api_call_test.py
@@ -9,11 +9,27 @@ import requests
 from requests.auth import HTTPBasicAuth
 from ipywidgets.embed import embed_minimal_html
 from IPython.display import display
+import json
 #input your apikey here... not sure if there is any safety issues of putting the api key into github, will look
 ## into but for now im not gonna put it in.
 apiKey = 'AOSIIFR5L7HM6KUISW2D4UFK'
 mapKey = 'AIzaSyC3MDZ1-SOhalWrHhcz_o9WlgePVL_NYTI'
 gmaps.configure(api_key=mapKey)
+
+
+#updates the list of devices in the area. 
+def update():
+
+    sn = []
+    auth = HTTPBasicAuth(apiKey,"")
+    req1 = requests.request("get","https://api.quant-aq.com/device-api/v1/orgs/1212/networks", headers = None, auth = auth)
+    f = open("exe.json","w")
+    data = req1.json()
+    sn = data["data"][0]["devices"]
+    return sn
+
+
+
 #return a df
 def fetchAllRecent(client):
 
@@ -26,7 +42,7 @@ def fetchAllRecent(client):
     #devices = fetchActiveDevices(client)
     data = []
     # get every serial number from devices
-    sn = ['MOD-PM-00681', 'MOD-PM-00673','MOD-PM-00682','MOD-PM-00676','MOD-PM-00665']
+    sn = update()
     
     # #loop through each device and call data list limited to the most recent 1. 
     for i in sn:
@@ -36,7 +52,7 @@ def fetchAllRecent(client):
 
     #turns list of dataframes into 1 data fram i think.... this is the part that probably went wrong
     data = pd.concat(data, ignore_index=True)
-    # data = data[['geo.lat', 'geo.lon','pm10']]
+    data = data[['sn','geo.lat', 'geo.lon','pm25', 'pm10']]
     return data
 
 
@@ -56,37 +72,7 @@ def getAllLatitudeLongitude(client):
 #get the client
 
 client = quantaq.QuantAQAPIClient(api_key = apiKey)
-who = client.whoami()
-print(who)
-print(getLongitudeLatitude(client, "MOD-PM-00710"))
-# print(client.teams.list())
 devices = to_dataframe(client.devices.list())
-# print(devices.columns)
-# print(devices.loc['MOD-PM-00696'])
 data = fetchAllRecent(client)
-# # data = data.div([1,1,100],axis = 'columns')
-# print(data.columns)
-
-url = 'https://api.quant-aq.com/device-api/v1/orgs/'
-# headers = {'Accept': 'application/json'}  
-auth = HTTPBasicAuth(apiKey,"")
-req = requests.request("get", url,headers = None,auth = auth)
-print(req.json())
-# print(req.json())
-req1 = requests.request("get","https://api.quant-aq.com/device-api/v1/orgs/1212/networks", headers = None, auth = auth)
-f = open("exe.json","w")
-import json
-data = req1.json()
-with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
-f.close()
-# fig = gmaps.figure()
-# heatmap_layer = gmaps.heatmap_layer(data[['geo.lat', 'geo.lon']], weights=data['pm10'],max_intensity=10,point_radius=300)
-# print(data)
-# # fig = gmaps.figure(zoom_level=12)
-
-# # display(fig)
-# fig.add_layer(heatmap_layer)
-# display(fig)
-# embed_minimal_html('export.html', views=[fig])
+print(data)
 
