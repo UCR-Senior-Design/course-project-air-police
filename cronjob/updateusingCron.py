dffa7d5c1e81e64f3399bfe9e51ecbea31116922
Joseph Chang <jchan443@ucr.edu>
2024-03-01 02:05:24 -0800

    whoops forgot the check loop

--- cronjob/updateusingCron.py
+++ cronjob/updateusingCron.py
@@ -10,5 +10,10 @@ import time
 
 
 schedule.every().day.at("8:00").do(push.updateDBs())
+schedule.run_pending()
 
+while True:
+    # checks every hour for a pending job
+    time.sleep(3600)
+    schedule.run_pending()
 # set up cron job 1 here
