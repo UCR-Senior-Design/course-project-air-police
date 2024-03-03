c36d8bbb532d6f0480398bdd2c5e38223f51ae43
Harry64C <harrisoncooper02@icloud.com>
2024-03-03 15:32:36 -0800

    improved table and website UI

--- routes/homeScript.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('viewMapButton').addEventListener('click', function () {
-    //Redirects to the map page
-    window.location.href = '/map';
-});
-
-document.getElementById('viewDataButton').addEventListener('click', function () {
-    //Redirects to the view data page
-    window.location.href = '/work-in-progress';
-});
