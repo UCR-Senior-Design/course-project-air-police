bafee563dd2b66134b600ba015112c8c1717f387
Joseph Chang <jchan443@ucr.edu>
2024-03-03 22:31:04 -0800

    handled logout invite button oh had to merge from main,

--- app.js
+++ app.js
@@ -431,7 +431,8 @@ app.use("/rlogin", rloginRouter);
 
 const tableRouter = require("./routes/table.js");
 app.use("/table", tableRouter);
-
+const lgRouter = require("./routes/logout.js");
+app.use("/logout", lgRouter);
 ////////////////////////////////////////////////////////////////
 const router = express.Router();
 
@@ -451,34 +452,73 @@ app.post("/data-analysis-testing", (req, res) => {
 });
 ///////////////////////////////////////////////////////
 
-
 //Route for handling form submission for data analysis testing
 app.post("/data-analysis-testing", (req, res) => {
   const enteredMonitorId = req.body.monitorId;
   const validMonitorIds = [
-    "MOD-PM-00645", "MOD-PM-00642", "MOD-PM-00636", "MOD-PM-00637", "MOD-PM-00651",
-    "MOD-PM-00665", "MOD-PM-00678", "MOD-PM-00687", "MOD-PM-00703", "MOD-PM-00691",
-    "MOD-PM-00695", "MOD-PM-00671", "MOD-PM-00676", "MOD-PM-00692", "MOD-PM-00661",
-    "MOD-PM-00166", "MOD-PM-00677", "MOD-PM-00704", "MOD-PM-00681", "MOD-PM-00688",
-    "MOD-PM-00682", "MOD-PM-00673", "MOD-PM-00672", "MOD-PM-00666", "MOD-PM-00656",
-    "MOD-PM-00654", "MOD-PM-00639", "MOD-PM-00662", "MOD-PM-00668", "MOD-PM-00655",
-    "MOD-PM-00709", "MOD-PM-00684", "MOD-PM-00674", "MOD-PM-00659", "MOD-PM-00653",
-    "MOD-PM-00641", "MOD-PM-00638", "MOD-PM-00635", "MOD-PM-00683", "MOD-PM-00711",
-    "MOD-PM-00640", "MOD-PM-00675", "MOD-PM-00646", "MOD-PM-00696", "MOD-PM-00652",
-    "MOD-PM-00660"
+    "MOD-PM-00645",
+    "MOD-PM-00642",
+    "MOD-PM-00636",
+    "MOD-PM-00637",
+    "MOD-PM-00651",
+    "MOD-PM-00665",
+    "MOD-PM-00678",
+    "MOD-PM-00687",
+    "MOD-PM-00703",
+    "MOD-PM-00691",
+    "MOD-PM-00695",
+    "MOD-PM-00671",
+    "MOD-PM-00676",
+    "MOD-PM-00692",
+    "MOD-PM-00661",
+    "MOD-PM-00166",
+    "MOD-PM-00677",
+    "MOD-PM-00704",
+    "MOD-PM-00681",
+    "MOD-PM-00688",
+    "MOD-PM-00682",
+    "MOD-PM-00673",
+    "MOD-PM-00672",
+    "MOD-PM-00666",
+    "MOD-PM-00656",
+    "MOD-PM-00654",
+    "MOD-PM-00639",
+    "MOD-PM-00662",
+    "MOD-PM-00668",
+    "MOD-PM-00655",
+    "MOD-PM-00709",
+    "MOD-PM-00684",
+    "MOD-PM-00674",
+    "MOD-PM-00659",
+    "MOD-PM-00653",
+    "MOD-PM-00641",
+    "MOD-PM-00638",
+    "MOD-PM-00635",
+    "MOD-PM-00683",
+    "MOD-PM-00711",
+    "MOD-PM-00640",
+    "MOD-PM-00675",
+    "MOD-PM-00646",
+    "MOD-PM-00696",
+    "MOD-PM-00652",
+    "MOD-PM-00660",
   ];
 
-  const isValidMonitorId = validMonitorIds.some(id => enteredMonitorId.toUpperCase() === id.toUpperCase());
+  const isValidMonitorId = validMonitorIds.some(
+    (id) => enteredMonitorId.toUpperCase() === id.toUpperCase(),
+  );
 
   if (isValidMonitorId) {
-      res.send("Correct Monitor ID. Proceed with data analysis.");
+    res.send("Correct Monitor ID. Proceed with data analysis.");
   } else {
-      //data-analysis-testing view with the error message
-      res.render("data-analysis-testing", { error: "Incorrect monitor ID. Please enter a valid Monitor ID." });
+    //data-analysis-testing view with the error message
+    res.render("data-analysis-testing", {
+      error: "Incorrect monitor ID. Please enter a valid Monitor ID.",
+    });
   }
 });
 
-
+//
 
 //////////////////////////////////////////////
 
