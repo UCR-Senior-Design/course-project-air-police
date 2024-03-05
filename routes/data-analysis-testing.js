1d2596e0cce2b011c85641c743f899fe714e4c68
Alex Hickerson <alexhickerson7@gmail.com>
2024-03-04 16:40:49 -0800

    Added router for success page

--- routes/data-analysis-testing.js
+++ routes/data-analysis-testing.js
@@ -1,6 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
 router.get('/data-analysis-testing', (req,res) => {
     const monitorId = req.body.monitorId;
 
     //For now, let's just render a page that displays a monitor ID prompt
     res.render("data-analysis", { monitorId: monitorId });
 })
+
+module.exports = router;
---
+++ routes/success-page.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req,res) => {
+    
+
+    //For now, let's just render a page that displays the success
+    res.render("success!", { title: 'SUCCESS PAGE ', displayText: 'participant login test' });
+    res.status(200);
+})
+
+module.exports = router;
