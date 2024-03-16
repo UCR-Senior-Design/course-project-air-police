4635ac3b754bf56d07d7e4f9f29b209c6349777e
Joseph Chang <jchan443@ucr.edu>
2024-03-16 14:46:20 -0700

    Update tests.cy.js

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -37,9 +37,39 @@ describe("home page", () => {
       expect(loc.pathname.toString()).to.contain("/table");
     });
   });
-});
-describe("visually", () => {
-  it("passes", () => {
+  it(" researcher login should not redirect work if user does not exists", () => {
     cy.visit("localhost:3000");
+    cy.get(".nav-link").click();
+    cy.get(":nth-child(2) > .dropdown-item").should("exist");
+    cy.get(":nth-child(2) > .dropdown-item").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname.toString()).to.contain("/rlogin");
+    });
+    cy.get("#username").type("dsnionoew");
+    cy.get("#password").type("1234");
+    cy.get("#submit").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname.toString()).to.contain("/rlogin");
+    });
+  });
+  it("table contains 2 tables logout button should be visible ( and invite)", () => {
+    cy.visit("localhost:3000");
+    cy.get(".nav-link").click();
+    cy.get(":nth-child(2) > .dropdown-item").should("exist");
+    cy.get(":nth-child(2) > .dropdown-item").click();
+    cy.location().should((loc) => {
+      expect(loc.pathname.toString()).to.contain("/rlogin");
+    });
+    cy.get("#username").type("pyTest");
+    cy.get("#password").type("1234");
+    cy.get("#submit").click();
+
+    cy.location().should((loc) => {
+      expect(loc.pathname.toString()).to.contain("/table");
+    });
+    cy.get(":nth-child(3) > .dropdown-item").should("exist");
+    cy.get(":nth-child(4) > .dropdown-item").should("exist");
+    cy.get("#errorTable").should("exist");
+    cy.get("#normalTable").should("exist");
   });
 });
