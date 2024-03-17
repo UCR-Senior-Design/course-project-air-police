0569490170ce2a3a1a0ea7c293baedddfc4a9b5f
cgeor015 <cgeor015@ucr.edu>
2024-03-17 02:31:17 -0700

    success page shows up at least... but aqi values dont show up. same with the image

--- routes/success-page.js
+++ routes/success-page.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express')
 const router = express.Router()
 
@@ -78,4 +79,68 @@ router.get('/', (req,res) => {
 })
 
 module.exports = router;
+*/
+const express = require('express');
+const router = express.Router();
+const { PythonShell } = require("python-shell");
 
+async function calculateAQI(desc) {
+    return new Promise((resolve, reject) => {
+        let options = {
+            mode: 'text',
+            pythonPath: '.venv/Scripts/python',
+            pythonOptions: ['-u'],
+            args: [desc]
+        };
+
+        PythonShell.run('data_call/aqi.py', options, (err, result) => {
+            if (err) reject(err);
+            resolve(result);
+        });
+    });
+}
+
+async function makeImgSRC(monitorId) {
+    return new Promise((resolve, reject) => {
+        let options = {
+            mode: "text",
+            pythonPath: ".venv/Scripts/python",
+            pythonOptions: ["-u"], // get print results in real-time
+            args: [monitorId],
+        };
+        PythonShell.run("data_call/aqi.py", options).then((result) => {
+            let img_src = "data:image/png;base64,";
+            img_src = img_src.concat(result);
+            resolve(img_src);
+        }).catch(reject);
+    });
+}
+
+router.get('/', async (req, res) => {
+    const monitorId = req.query.monitorId;
+    /* //attempting to make this work
+    try {
+        const [img_src, aqi] = await Promise.all([
+            makeImgSRC(monitorId),
+            calculateAQI(monitorId)
+        ]);
+
+        const aqiValues = JSON.parse(aqi);
+
+        res.render('success-page', { title: 'SUCCESS PAGE', monitorId, img_src, aqi: aqiValues });
+    } catch (error) {
+        console.error('Error calculating AQI:', error);
+        res.status(500).send('Error calculating AQI');
+    }
+    */
+    try {
+        const img_src = await makeImgSRC(monitorId);
+        res.render('success-page', { title: 'SUCCESS PAGE', monitorId, img_src });
+    } catch (error) {
+        console.error('Error calculating AQI:', error);
+        res.status(500).send('Error calculating AQI');
+    }
+
+});
+
+module.exports = router;
