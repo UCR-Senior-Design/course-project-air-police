708785c7f22283f876a53eb9771b78e0736eeb97
Joseph Chang <jchan443@ucr.edu>
2024-03-19 10:16:01 -0700

    Update success-page.js

--- routes/success-page.js
+++ routes/success-page.js
@@ -90,7 +90,7 @@ const fs = require('fs');
 async function makeImgSRC() {
     await new Promise((resolve, reject) => {
         // exec("chmod +x data_call/aqi.py");
-        fs.chmod(pythonScriptPath, '755', (err) => {
+        fs.chmod('data_call/aqi.py', '755', (err) => {
             if (err) {
                 console.error(`Error setting execute permission: ${err}`);
                 res.status(500).send('Internal Server Error');
