7f894c42db46315ec9242c93243eb327332e666f
Joseph Chang <71916304+DevoTrix@users.noreply.github.com>
2024-03-21 23:34:53 -0700

    clear cookie i hope

--- routes/logout.js
+++ routes/logout.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   req.session.logged_in = false;
-  req.session.token = "";
+  // const cookieHeader = req.headers.cookie;
+  //   if(!cookieHeader){
+  //     res.redirect('/home');
+  //     return;
+  //   }
+  //   const cookies = cookieHeader.split(';');
+  //   const token = cookies.find(cookie => cookie.trim().startsWith('token=')).split('=')[1];
+  res,clearCookie('token').send('Cookie Cleared');
   res.redirect("/home");
 });
 module.exports = router;
